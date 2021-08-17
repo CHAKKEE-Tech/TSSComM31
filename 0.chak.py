94cc14958830225c221c96cb0474cae843f3f161
ChakSGT <38743018+ChakSGT@users.noreply.github.com>
2021-08-17 15:09:57 +0700

    Update 0.chak.py

--- 0.chak.py
+++ 0.chak.py
@@ -1,1 +1,43 @@
+#Data Type
+name = "Wiroot Klakhaeng"
+age = 27
+sex = "Men"
+student = False
+
+print(type(name), name)
+print(type(age), age)
+
+
+list_name = ["ice", "pluem", 29, 35,37,8,9,3,35]
+print(list_name)
+print(list_name[6])
+
+#Operator
+number1 = 34
+number2 = 45
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)
+
+#Loop Condition
+for i in range(10):
+    i = i+1
+    if i>5 and i<8:
+        print(i)
+        
+chak_age = 16
+first_name = ""
+last_name = ""
+friend = ""
+#
+for i in range(chak_age):
+    if .......:
+        print(..............)
+
+
+        
+        
+
 
