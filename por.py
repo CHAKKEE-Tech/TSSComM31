f7f5a16dfaea0a4ee52e9047f78d1328dad7ba72
shiraori000 <88703887+shiraori000@users.noreply.github.com>
2021-08-17 15:46:06 +0700

    Update por.py

--- por.py
+++ por.py
@@ -1,1 +1,36 @@
+#Data Type
+name = "Por Trakoonsin"
+age = 14
+sex = "Men"
+student = True
+list_name = ["Por", 1]
+list_friend = ["Mac", "Copter", "Long", "Music", "Tprem"]
 
+print(type(name), name)
+print(type(age), age)
+print(type(sex), sex)
+print(type(student), student)
+print(list_name)
+print(list_friend[4])
+
+#Operator
+x = 25
+y = 60
+print(x-y)
+print(x*y)
+print(x-y)
+print(x/y)
+
+#Loop Condition
+for i in range(5):
+    i = i+1
+    if i> 2:
+        print(i)
+
+Por_age = 14
+first_name = "Por"
+last_name = "Trakoonsin"
+friend = "Music"
+for i in range(Por_age):
+    if i > 9:
+        print(i,first_name,last_name,friend)
