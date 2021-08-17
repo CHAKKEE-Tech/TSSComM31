264ec620683ab9bd117165d1c4c0682eab082249
Mudmee2 <88703548+Mudmee2@users.noreply.github.com>
2021-08-17 15:49:20 +0700

    Update 16.pranpreeya.py

--- 16.pranpreeya.py
+++ 16.pranpreeya.py
@@ -1,1 +1,39 @@
+#Data Type
+name = "Pranpreeya Srikong"
+age = 14
+sex = "Women"
+student = True
+list_name = ["ice", "pluem",]
+list_friends = ["ice", "pluem", "eva", "aomsin", "namnueng"]
+print(type(name), name) 
+print(type(age), age)
+print(type(sex), sex)
+print(type(student), student)
+print(list_name[0])
+print(list_friends)
+
+#operator
+number1 = 32
+number2 = 54
+print(number1+number2)
+print(number1-number2)
+print(number1*number2)
+print(number1/number2)
+print(number1%number2)
+
+#loop Condition
+for i in range(10):
+    i = i+1
+    print(i)
+    if i>5 and i<8:
+        print(i)
+  
+       
+mudmee_age = 14
+first_name ="Pranpreeya"
+last_name ="Srikong"
+friend ="Eva"
+for i in range(mudmee_age):
+   if i>9:
+     print(i, first_name, last_name, friend)
 
