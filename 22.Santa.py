ad5cd80650c95c7899b3ec27d0a68d6451fb31cd
Phumipatsawaspol <88702869+Phumipatsawaspol@users.noreply.github.com>
2021-08-17 15:24:30 +0700

    Update 22.Santa.py

---
+++ 22.Santa.py
@@ -0,0 +1,34 @@
+#Data Type
+name = "Phumipat Sawaspol"
+age = 15
+Sex = "Men"
+student = True
+list_name = ["ice","Plume","Vigo","Max","Music"]
+
+print(type(name),name)
+print(type(age),age)
+print(type(Sex),Sex)
+print(type(student),student)
+print(list_name[3])
+
+# Operator
+number1 = 34
+number2 = 45
+print(number1 + number2 - number1 * number2 / number1)
+
+#Loop Condition
+for i in range(10):
+    i = i+1
+    if i>5 and i<=8:
+        print(i)
+        
+Phumipat_Sawaspol = 15
+first_name = "Phumipat"
+last_name = "Sawaspol"
+Friend = "Vigo"
+for i in range(Phumipat_Sawaspol):
+        if i>=10:
+            print(i)
+            print(first_name)
+            print(last_name)
+            print(Friend)
