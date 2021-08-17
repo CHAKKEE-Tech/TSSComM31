12a50476772cece51844dd119276be64675eb9dd
vigrooo <88703840+vigrooo@users.noreply.github.com>
2021-08-17 15:20:58 +0700

    Update 17.vigo.py

--- 17.vigo.py
+++ 17.vigo.py
@@ -1,1 +1,38 @@
+#Data Type
+name = "Pacharadanai Tonhsirithongbai"
+age = 14
+sex = "Men"
+student = True
+list_name = ["vigo", 1]
+list_Friend = ["santa","pluem","max","brave","mudmee"]
 
+print(type(name), name)
+print(type(age), age)
+print(type(sex), sex)
+print(type(student), student)
+print(list_name)
+print(list_Friend)
+print(list_Friend[4])
+
+#Operator
+num1 = 12
+num2 = 20
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+#Loop Condition
+for i in range(10):
+    print(i)
+       
+       
+
+vigo_age = 14
+first_name= "Pacharadanai"
+last_name = "Tongsirithongbai"
+friend = "santa"
+for i in range(vigo_age):
+    if i>=10:
+     print(i,first_name,last_name,friend)
+    
