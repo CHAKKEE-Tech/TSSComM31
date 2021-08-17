a785d6a9df8426825c19ab9cf86d529df4d75e10
Braved <88700299+Braved@users.noreply.github.com>
2021-08-17 15:16:35 +0700

    Update 3.Brave.py

--- 3.Brave.py
+++ 3.Brave.py
@@ -1,1 +1,34 @@
+#Data Type
+name = "Jeerapat Jaroenniwetnukul"
+age = 14
+sex = "Men"
+student = False
+list_friend = ["ice","pleum","yuta","mac","long"]
 
+print(type(name), name)
+print(type(age), age)
+print(type(sex), sex)
+print(type(student), student)
+print(list_friend)
+print(list_friend[3])
+
+#Operator
+number1 = 69
+number2 = 32
+
+print(number1+number2)
+print(number1-number2)
+print(number1*number2)
+print(number1/number2)
+
+#Loop Condition
+    
+Brave_age = 14    
+first_name = "Jeerapat"
+last_name = "Jaroenniwetnukul"
+friend = "Pleum"
+
+for i in range(Brave_age) :
+    if i>9:
+        print(i,first_name,last_name,friend)
+    
