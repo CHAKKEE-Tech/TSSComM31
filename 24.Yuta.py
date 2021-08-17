107c9cc0e5d8b66a7983e88d9a505e625b840bf7
matanyuta <88703367+matanyuta@users.noreply.github.com>
2021-08-17 15:28:09 +0700

    Create 24.Yuta.py

---
+++ 24.Yuta.py
@@ -0,0 +1,33 @@
+
+name = "Matanyuta Soontornpalin"
+age = 14
+sex = "Woman"
+student = "True"
+list_friend = ["Noon","Ploy","Brave","Mac","Copter"]
+
+print(type(name), name)
+print(type(age), age)
+print(type(sex), sex)
+
+print(list_friend[4])
+
+number1 = 30
+number2 = 40
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
+for i in range(10):
+    i = i+1
+    if i>5 and i<=8:
+      print(i)
+      
+Yuta_age = 14
+First_name = "matanyuta"
+Last_name = "Soontornpalin"
+Friend = "Noon"
+list_name = [""]
+for i in range(Yuta_age):
+    if i>=10:
+      print (i,First_name,Last_name,Friend)
