1350b725ddeeb5a22713fb29c0f59f010a441128
Premmy191 <88702896+Premmy191@users.noreply.github.com>
2021-08-17 15:44:46 +0700

    Update 9.techin.py

---
+++ 9.techin.py
@@ -0,0 +1,27 @@
+name = "prem"
+age = 15
+sex = "men"
+
+print(type(name),name)
+list_name = ["ice","pleum","prem","mac","santa"]
+
+print(type(name),name)
+print(list_name)
+print(list_name[4])
+
+number1 = 34
+number2 = 45
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)
+
+prem_age=15
+first_name="techin"
+last_name="jitthaisong"
+friend = "vigo"
+
+for i in range(prem_age):
+    if i>9:
+      print(i,first_name,last_name,friend)
