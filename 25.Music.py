ce2ece957fbeeb6cdefd576ad81f97d603c34856
Suprawee123 <88703013+Suprawee123@users.noreply.github.com>
2021-08-17 15:45:46 +0700

    Create 25.Music.py

---
+++ 25.Music.py
@@ -0,0 +1,35 @@
+#Data Type
+name = "Suprawee Pirompanich"
+age = 15
+sex = "Men"
+student = True
+list_friends = ["Por","Mac","Long","Vigo","Tprem"]
+
+print(type(name),name)
+print(type(age),age)
+print(type(sex),sex)
+print(type(student),student)
+print(list_friends)
+print(list_friends[4])
+
+#Operator
+x = 25
+y = 60
+print(x+y)
+print(x*y)
+print(x-y)
+print(x/y)
+
+#Loop Condition
+for i in range(5):
+    i = i+1
+    if i> 2:
+         print(i)
+         
+Music_age = 15
+first_name = "Suprawee"
+last_name = "Pirompanich"
+friend = "Por"
+for i in range(Music_age):
+    if i > 9:
+        print(i,first_name,last_name,friend)
