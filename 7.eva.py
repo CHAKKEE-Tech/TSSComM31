474ca2b74ea5b8f43f0987f7833ce4b6f2ddfeba
Nathashaeva <88703102+Nathashaeva@users.noreply.github.com>
2021-08-18 21:27:16 +0700

    Update 7.eva.py

--- 7.eva.py
+++ 7.eva.py
@@ -1,1 +1,37 @@
+#Data Type
+name = "Nathasha Potipassa"
+age = 14
+sex = "Woman"
+student = True
+list_name = ["Aomsin","Mudmee"]
+list_friends =["Aomsin","Mudmee","Mew","Namnueng","Eve"]
+print(type(name),name)
+print(type(age),age)
+print(type(sex),sex)
+print(type(student),student)
+print(list_name[0])
+print(list_friends)
 
+#Operator
+number1 = 32
+number2 = 54
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)
+
+#loop condition
+for i in range(10):
+    i = i+1
+    if i>5 and i<8:
+        print(i)
+        
+eva_age = 14
+first_name = "Nathasha"
+last_name = "Potipassa"
+friend = "Mew"
+for i in range(eva_age):
+      if i>9:
+          print(i,first_name,last_name,friend)
+  
