7f3ff9b5802acf0dbd9acec9b21c463d62298cbb
maitammada <88702842+maitammada@users.noreply.github.com>
2021-08-18 20:45:00 +0700

    Create 18.mac.py

---
+++ 18.mac.py
@@ -0,0 +1,30 @@
+#Data Type
+name = "polavat sirithanateerapatch"
+age = 14
+sex = "male"
+student = True
+list_name = ["ice","pleum","ploy","yuta","copter"]
+print(type(name),name)
+print(type(age),age)
+print(type(sex),sex)
+print(type(student),student)
+print(list_name)
+print(list_name[4])
+
+#Operator
+number1 = 25
+number2 = 15
+print(number1+number2-number1/number2)
+
+#Loop Condition
+for i in range(10):
+    i=i+1
+    print(i)
+
+mac_age = 14
+first_name = "polavat"
+last_name = "sirithanateerapatch"
+friend = "copter"
+for i in range(14):
+    if i>10:
+        print(i,first_name,last_name,friend)
