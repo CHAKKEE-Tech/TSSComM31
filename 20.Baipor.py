9ee5cb2080c8a3370bdc60e6958233e40703b466
20.baipor <88703063+baipasaa@users.noreply.github.com>
2021-08-20 19:06:32 +0700

    Create 20.Baipor.py

---
+++ 20.Baipor.py
@@ -0,0 +1,46 @@
+#Data type
+name = "Pasawee Treewiroj"
+age = 13
+sex = "female"
+student = True
+list_name = ["baipor", 1]
+list_freind = ["ice", "tonkla", "mudmee", "brave", "eve"]
+
+print(type(name), name)
+print(type(age), age)
+print(type(sex), sex)
+print(type(student), student)
+print(list_name)
+print(list_freind[4])
+
+#Operator
+number1 = 24
+number2 = 26
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
+#loop condition
+for i in range(10)
+    i = i+1
+    if i>5 and i>8
+        print(i)
+    
+baipor_age = 13
+first_name = "Pasawee"
+last_name = "Treewiroj"
+freind = "ice"
+#
+
+for i in range(baipor_age)
+    if i>5 and i>8
+    print(type(13), 13)
+    print(type(Pasawee), Pasawee)
+    print(type(Treewiroj), Treewiroj)
+    print(type(ice), ice)
+    
+#Operator
+number1 = 24
+number2 = 26
+print(number1 + number2)
