58c5b9fe04da9a834b4009fac078182de0929e15
kittnat <88702967+kittnat@users.noreply.github.com>
2021-08-20 19:55:49 +0700

    Update 1.ice.py

--- 1.ice.py
+++ 1.ice.py
@@ -1,1 +1,38 @@
+#Data type
+name = "Kittnat Anumardchimplee"
+age = 13
+sex = "female"
+student = True
+list_name = ["ice", 1]
+list_freind = ["baipor", "tonkla", "mudmee", "brave", "eve"]
 
+print(type(name), name)
+print(type(age), age)
+print(type(sex), sex)
+print(type(student), student)
+print(list_name)
+print(list_freind[4])
+
+#Operator
+number1 = 24
+number2 = 26
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
+#loop condition
+for i in range(10)
+    i = i+1
+    if i>5 and i>8
+        print(i)
+    
+ice_age = 14
+first_name = "Kittnat"
+last_name = "Anumardchimplee"
+freind = "baipor"
+#
+
+for i in range(ice_age)
+    if i>5 and i>8
+    print(type(13), 13)
